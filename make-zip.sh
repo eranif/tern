5ff785f6f44eedef0c66b584ce8dd19390e21dad
Eran <eran.ifrah@gmail.com>
2015-04-02 10:58:58 +0300

    updated packaing tool to include the specific files per OS

--- make-zip.sh
+++ make-zip.sh
@@ -4,17 +4,25 @@ OS=$1
 # Windows
 rm -f .gitattributes
 echo "node.osx export-ignore" > .gitattributes
-git archive --format=zip --output=javascript-win.zip master
-exit
+echo "Making ZIP for OSX..."
+cat .gitattributes
+git archive --worktree-attributes --format=zip --output=javascript-win.zip master
 
 # OSX
+
 rm -f .gitattributes
 echo "node.exe export-ignore" > .gitattributes
-git archive --format=zip --output=javascript-osx.zip master
+echo "Making ZIP for OSX..."
+cat .gitattributes
+git archive --worktree-attributes --format=zip --output=javascript-osx.zip master
 
 
 # Linux
 rm -f .gitattributes
+
+echo "Making ZIP for Linux..."
 echo "node.exe export-ignore" > .gitattributes
 echo "node.osx export-ignore" >> .gitattributes
-git archive --format=zip --output=javascript-linux.zip master
+cat .gitattributes
+
+git archive --worktree-attributes --format=zip --output=javascript.zip master
