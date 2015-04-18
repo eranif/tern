847c9f5ddd49802606a1cab423285b1fa8cbf12f
Eran <eran.ifrah@gmail.com>
2015-04-18 17:33:03 +0300

    update ZIP making script

--- make-zip.sh
+++ make-zip.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 OS=$1
 
+# rebase
+echo "Rebasing against original git repo..."
+git fetch upstream
+git rebase upstream/master
+
 # Windows
 rm -f .gitattributes
+rm -f javascript-win.zip
+
 echo "node.osx export-ignore" > .gitattributes
 echo "Making ZIP for OSX..."
 cat .gitattributes
@@ -11,6 +18,7 @@ git archive --worktree-attributes --format=zip --output=javascript-win.zip maste
 # OSX
 
 rm -f .gitattributes
+rm -f javascript-osx.zip
 echo "node.exe export-ignore" > .gitattributes
 echo "Making ZIP for OSX..."
 cat .gitattributes
@@ -19,6 +27,7 @@ git archive --worktree-attributes --format=zip --output=javascript-osx.zip maste
 
 # Linux
 rm -f .gitattributes
+rm -f javascript.zip
 
 echo "Making ZIP for Linux..."
 echo "node.exe export-ignore" > .gitattributes
@@ -26,3 +35,6 @@ echo "node.osx export-ignore" >> .gitattributes
 cat .gitattributes
 
 git archive --worktree-attributes --format=zip --output=javascript.zip master
+
+echo "Copying ZIP files to CodeLite's tree"
+cp *.zip ../codelite/WebTools/
