ee5b880642c1d3961744af8bfb7acd51384707e0
Eran Ifrah <eran@eran-debian.eran-debian>
2016-01-18 09:53:03 +0200

    fixed wrong paths

--- plugin/node-express.js
+++ plugin/node-express.js
@@ -1,8 +1,8 @@
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    return mod(require("tern/lib/infer"), require("tern/lib/tern"));
+    return mod(require("../lib/infer"), require("../lib/tern"));
   if (typeof define == "function" && define.amd) // AMD
-    return define([ "tern/lib/infer", "tern/lib/tern" ], mod);
+    return define([ "../lib/infer", "../lib/tern" ], mod);
   mod(tern, tern);
 })(function(infer, tern) {
   "use strict";
